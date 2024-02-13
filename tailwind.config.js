46e0fe9202758a3f6010fd50446d6cd5c07aa5bd
Geran <113444430+gurainzu@users.noreply.github.com>
2024-02-13 18:44:59 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': "Poppins",
+      },
+      backgroundImage: {
+        'pattern1': "url('/media/pattern1.svg')",
+        'pattern2': "url('/media/pattern2.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
