a6d9f2cdc16ae76d23e456f7b93dc9ef03d85aa3
Geran <113444430+gurainzu@users.noreply.github.com>
2024-02-18 01:10:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -17,16 +17,75 @@ function showSlides() {
 
 document.addEventListener('DOMContentLoaded', function () {
   const navLinks = document.querySelectorAll('nav a');
+  const navBurger = document.querySelectorAll('nav div');
 
   navLinks.forEach(function (navLink) {
     const initialClass = navLink.className;
 
     window.addEventListener('scroll', function () {
       if (window.scrollY > 500) {
-        navLink.className = initialClass + ' bg-black/25 backdrop-blur-xl rounded-lg';
+        navLink.className = initialClass + ' bg-black/25 backdrop-blur-xl rounded-lg hidden';
       } else {
         navLink.className = initialClass;
       }
     });
   });
+  
+  navBurger.forEach(function (navBurger) {
+    const initialClass = navBurger.className;
+
+    window.addEventListener('scroll', function () {
+      if (this.window.scrollY > 500) {
+        navBurger.className = initialClass - 'lg:hidden';
+        navBurger.className = initialClass + 'justify-end';
+      } else {
+        navBurger.className = initialClass;
+      }
+    });
+  });
+
+  navBurger.addEventListener("click,", function() {
+    if (navLinks.classList.contains("hidden")) {
+      navLinks.classList.remove("hidden");
+    } else {
+      navLinks.classList.remove("hidden");
+    }
+  })
 });
+
+// Poster //
+
+const postImage = document.getElementById("post");
+const expandButton = document.getElementById("expand-button");
+const container = document.getElementById("containerpost");
+
+postImage.addEventListener("click", function() {
+    if (postImage.classList.contains("max-h-full")) {
+        postImage.classList.remove("max-h-full");
+        postImage.classList.add("max-h-96");
+        container.style.maxWidth = "300px";
+    } else {
+        postImage.classList.remove("max-h-96");
+        postImage.classList.add("max-h-full");
+
+        // Get the image's width and set the container's width
+        const imageWidth = postImage.naturalWidth;
+        container.style.maxWidth = `${imageWidth}px`;
+    }
+});
+
+expandButton.addEventListener("click", function() {
+    if (postImage.classList.contains("max-h-full")) {
+        postImage.classList.remove("max-h-full");
+        postImage.classList.add("max-h-96");
+        container.style.maxWidth = "300px";
+    } else {
+        postImage.classList.remove("max-h-96");
+        postImage.classList.add("max-h-full");
+
+        // Get the image's width and set the container's width
+        const imageWidth = postImage.naturalWidth;
+        container.style.maxWidth = `${imageWidth}px`;
+    }
+});
+
