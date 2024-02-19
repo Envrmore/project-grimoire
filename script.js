b70d83e4bb002c40e816300a8571f67ef2ffb050
Geran <113444430+gurainzu@users.noreply.github.com>
2024-02-20 06:19:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Image Slider //
+
 var slideIndex = 0;
 showSlides();
 
@@ -87,3 +89,25 @@ expandButton.addEventListener("click", function() {
     }
 });
 
+// Back to top //
+
+const mybutton = document.getElementById("backtotop");
+
+const scrollFunction = () => {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.classList.remove("hidden");
+    } else {
+      mybutton.classList.add("hidden");
+    }
+  };
+  const backToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+  
+  window.addEventListener("scroll", scrollFunction);
